49407356c395e7f3db3530080de982a7ffc64a6c
Markus Tacker <m@coderbyheart.com>
2019-09-04 16:50:20 +0200

    test: iteration 1

---
+++ character.spec.js
@@ -0,0 +1,11 @@
+describe('Character', () => {
+    it.todo('has health, starting at 1000')
+    it.todo('has a level, starting at 1')
+    it.todo('can be dead or alive')
+    it.todo('can deal damage')
+    it.todo('can heal')
+    test.todo('health becomes 0 if damage is greater than health')
+    it.todo('dies when health is 0')
+    it.todo('cannot be healed if it is dead')
+    it.todo('cannot be healed over 1000')
+})
